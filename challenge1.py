66dff7c084592e4cf4a38646a48c4a9ad39e158d
Luke Harper <lukeharper@live.ca>
2016-09-18 22:48:59 -0400

    Cypher Solved

--- challenge1.py
+++ challenge1.py
@@ -6,8 +6,16 @@ import re
 
 PATTERN_CHUNK = 4
 
+
+# Factor formula found from:
+# http://stackoverflow.com/questions/6800193/what-is-the-most-efficient-way-of-finding-all-the-factors-of-a-number-in-python
+def factors(n):
+    return set(reduce(list.__add__,
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+
 # function to rotate letters in text based on some key, Dec is boolean to decrement or not
-def rotText(text, key, dec=False):
+def rotText(text, key, decrypt=False):
     word = ""
     it = 0
     for c in text:
@@ -17,10 +25,10 @@ def rotText(text, key, dec=False):
         # getting the key
         k = key[it]
 
-        if dec:
-            word += chr(97 + ((ord(c) - ord(k)) % 26))
+        if decrypt:
+            word += chr(97 + (((ord(c) - 97) - (ord(k) - 97)) % 26))
         else:
-            word += chr(97 + ((ord(c) + ord(k)) % 26))
+            word += chr(97 + (((ord(c) - 97) + (ord(k) - 97)) % 26))
 
         # Incrementing the Iterator and wrapping if we go over
         it = (it + 1) % len(key)
@@ -47,7 +55,32 @@ def lengthBetween(aSet, text):
         firstPos = text.index(matches[0])
         secondPos = text.index(matches[0], firstPos + PATTERN_CHUNK)
         distanceList.append(secondPos - firstPos)
-    return distanceList
+    return Set(distanceList)
+
+
+def getEstimatedKeyLength(text):
+    repLengthList = []
+    for repLength in lengthBetween(findPattern(text), text):
+        repLengthList.extend(factors(repLength))
+
+    # Tally up possible key lengths
+    return Counter(repLengthList)
+
+# Loops through a piece of text and making a list of strings of size, with spaces not counted as a part of size
+def chunkText(text, size):
+    chunks = []
+    aChunk = ""
+    i = 0
+    for c in text:
+        aChunk += c
+        if c is not ' ':
+            i += 1
+        if i == size:
+            chunks.append(aChunk)
+            aChunk = ""
+            i = 0
+
+    return chunks
 
 
 
@@ -61,22 +94,56 @@ with open(filename, 'r') as infile:
 # print cipherText
 wordbank = cipherText.split()
 letterBank = cipherText.replace(' ', '')
-print lengthBetween(findPattern(letterBank), letterBank)
+estimatedKeyLengthCount = getEstimatedKeyLength(letterBank).most_common()
 
-# print wordbank
+# Filtering out too weak keys, basically anything that is size 3 or under
+estimatedKeyLengthCount = filter(lambda x: x[0] > 3, estimatedKeyLengthCount)
+print "Estimated Keys"
+print estimatedKeyLengthCount
+# Getting the most common size
+estimatedKeyA = estimatedKeyLengthCount[0][0]
+estimatedKeyB = estimatedKeyLengthCount[1][0]
+
+#Dividing up our message into characters of these chunks,
+# leaving spaces in to identify short words to be able to run rotation tests on for frequency of letters
+chunkList = chunkText(cipherText, estimatedKeyA)
+chunkListB = chunkText(cipherText, estimatedKeyB)
+print "CHUNK LIST"
+print chunkList
+print chunkListB
+
 freqWord = Counter(wordbank)
 freqLetter = Counter(letterBank)
 cipherLength = len(letterBank)
-# print freqLetter
-# print cipherLength
 letters = freqLetter.most_common(3)
 
 # for letter in letters:
 #     print rotText(letter[0], "e", True)
-list = list(freqWord)
-# print list
-# for w in list:
-#     if len(w) == 1:
-#         print rotText(w, "a", True)
 
-# print rotText(cipherText, "nsug")
+threeLetters = []
+singles = []
+# print list
+for w in wordbank:
+    if len(w) == 3:
+        threeLetters.append(rotText(w, "the", True))
+    if len(w) == 1:
+        singles.append((w, rotText(w, "I", True)))
+print 'SINGLES: '
+print singles
+print '3 Letter Counts: '
+print Counter(threeLetters).most_common()
+# get the letters out of the string of "the"
+charList = []
+for w in threeLetters:
+    for c in w:
+        charList.append(c)
+print 'MOST COMMON CHARS IN 3 LETTERS'
+print Counter(charList).most_common()
+
+print 'ORIGINAL TEXT'
+print cipherText
+print 'DECODED TEXT'
+print rotText(cipherText, "rbga", True)
+print rotText(cipherText, "aagaraaa", True)
+print rotText(cipherText, "sugaraaa", True)
+print rotText(cipherText, "sugarbun", True)
